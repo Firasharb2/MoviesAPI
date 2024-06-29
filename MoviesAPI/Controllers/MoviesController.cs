411017f8483954d433fb583fb901f3def4f8214f
Firasharb2 <firas.harb2@gmail.com>
2024-06-29 16:18:24 +0300

    Add project files.

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _context;
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        { if(_context.Movies == null)
+            {
+                return NotFound();
+            }
+            return await _context.Movies.ToListAsync();
+        }
+
+        [Route("postMovie")]
+        [HttpPost]
+        
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+            return movie;
+        }
+    }
+
+
+}
---
+++ MoviesWebAPI.Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using MoviesWebAPI.Client.Models;
+using System.Net.Http.Json;
+
+public class Program
+{
+    public  static async Task Main(string[] args)
+    {Console.WriteLine("Please enter the movie you like to watch!");
+        string movie = Console.ReadLine();
+        Console.WriteLine("Please enter the genre of the movie you like to watch!");
+        string genre = Console.ReadLine();
+        Console.WriteLine("Please enter the release date of the movie you like to watch!");
+        DateTime releaseDate = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine("The movie you like to watch is: " + movie);
+        Console.WriteLine("The genre of the movie you like to watch is: " + genre);
+        Console.WriteLine("The release date of the movie you like to watch is: " + releaseDate);
+
+        Movie _movie = new Movie();
+        _movie.Title = movie;
+        _movie.Genre = genre;
+        _movie.ReleaseDate = releaseDate;
+
+        string baseUrl = "https://localhost:5021/api/movies";
+        var handler = new HttpClientHandler();
+        handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+        using (var client = new HttpClient(handler))
+        {
+            try
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                var response = await client.PostAsync(baseUrl, JsonContent.Create(_movie));
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Movie added successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to add movie");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+
+}
